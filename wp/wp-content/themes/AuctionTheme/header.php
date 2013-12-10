f9a4b6f2be820eabc1894e22466cba153c57871c
Stewart Harper <stewart.harper@gmail.com>
2013-12-09 19:03:21 -0800

    removed search bar

--- wp/wp-content/themes/AuctionTheme/header.php
+++ wp/wp-content/themes/AuctionTheme/header.php
@@ -244,123 +244,93 @@
 							</a>
 		        </div>
           </div> <!-- middle-header-bg -->
-            <div class="navbar-collapse collapse">
+          <div class="navbar-collapse collapse">
 
-              <!-- main menu place here -->
-              <?php
+            <!-- main menu place here -->
+            <?php
 
-                $AuctionTheme_show_main_menu = get_option('AuctionTheme_show_main_menu');
-                if($AuctionTheme_show_main_menu != 'no'):     
-                  $menu_name = 'primary-auction-main-header';
+              $AuctionTheme_show_main_menu = get_option('AuctionTheme_show_main_menu');
+              if($AuctionTheme_show_main_menu != 'no'):     
+                $menu_name = 'primary-auction-main-header';
 
-                if ( ( $locations = get_nav_menu_locations() ) && isset( $locations[ $menu_name ] ) ) {
-                  $menu = wp_get_nav_menu_object( $locations[ $menu_name ] );
-                  $menu_items = wp_get_nav_menu_items($menu->term_id);
-                  $m = 0;     
-                  foreach ( (array) $menu_items as $key => $menu_item ) {
-                    $title = $menu_item->title;
-                    $url = $menu_item->url;
-                    if(!empty($title))
-                    $m++;
-                  }}
-              ?>    
-            
-                <?php
-                if($m == 0):
-                ?>
-                   <ul class="nav navbar-nav navbar-right">
-                  <?php /*?>        
-                    <li><a href="<?php echo get_page_link(1375); ?>"><?php echo get_the_title(1375)?></a></li>  
-                  <?php */?>                    
-                  <li><a href="<?php echo get_page_link(1287); ?>"><?php echo get_the_title(1287)?></a></li>  
-                  <li><a href="<?php echo get_page_link(1282); ?>"><?php echo get_the_title(1282)?></a></li>  
-                  <li><a href="<?php echo get_page_link(1289); ?>"><?php echo get_the_title(1289)?></a></li>  
-                  <li><a href="<?php echo get_post_type_archive_link('auction'); ?>">All Properties</a></li>
-                  <li><a href="<?php echo get_page_link(1524); ?>"><?php echo get_the_title(1524)?></a></li>  
-                  <?php /*?>
-                    <li><a href="<?php echo get_permalink(get_option('AuctionTheme_adv_search_id')); ?>"><?php _e('Advanced Search','AuctionTheme'); ?></a></li>     
-                    <li><a href="<?php echo get_page_link(1264); ?>"><?php echo get_the_title(1264)?></a></li>     
-                  <?php */?>
-                </ul>
-                <?php else: 
-            
-                  $event = 'hover';
-                  $effect = 'fade';
-                  $fullWidth = ',fullWidth: true';
-                  $speed = 0;
-                  $submenu_width = 200;
-                  $menuwidth = 100;
+              if ( ( $locations = get_nav_menu_locations() ) && isset( $locations[ $menu_name ] ) ) {
+                $menu = wp_get_nav_menu_object( $locations[ $menu_name ] );
+                $menu_items = wp_get_nav_menu_items($menu->term_id);
+                $m = 0;     
+                foreach ( (array) $menu_items as $key => $menu_item ) {
+                  $title = $menu_item->title;
+                  $url = $menu_item->url;
+                  if(!empty($title))
+                  $m++;
+                }}
+            ?>    
           
-                ?>
-              
-                  <script type="text/javascript">
-                    var $ = jQuery;
-                    
-                    jQuery(document).ready(function($) {
-                      jQuery('#<?php echo 'item_main_menus'; ?> .menu').dcMegaMenu({
-                        rowItems: <?php echo $menuwidth; ?>,
-                        subMenuWidth: '<?php echo $submenu_width; ?>',
-                        speed: <?php echo $speed; ?>,
-                        effect: '<?php echo $effect; ?>',
-                        event: '<?php echo $event; ?>'
-                        <?php echo $fullWidth; ?>
-                      });
+              <?php
+              if($m == 0):
+              ?>
+                 <ul class="nav navbar-nav navbar-right">
+                <?php /*?>        
+                  <li><a href="<?php echo get_page_link(1375); ?>"><?php echo get_the_title(1375)?></a></li>  
+                <?php */?>                    
+                <li><a href="<?php echo get_page_link(1287); ?>"><?php echo get_the_title(1287)?></a></li>  
+                <li><a href="<?php echo get_page_link(1282); ?>"><?php echo get_the_title(1282)?></a></li>  
+                <li><a href="<?php echo get_page_link(1289); ?>"><?php echo get_the_title(1289)?></a></li>  
+                <li><a href="<?php echo get_post_type_archive_link('auction'); ?>">All Properties</a></li>
+                <li><a href="<?php echo get_page_link(1524); ?>"><?php echo get_the_title(1524)?></a></li>  
+                <?php /*?>
+                  <li><a href="<?php echo get_permalink(get_option('AuctionTheme_adv_search_id')); ?>"><?php _e('Advanced Search','AuctionTheme'); ?></a></li>     
+                  <li><a href="<?php echo get_page_link(1264); ?>"><?php echo get_the_title(1264)?></a></li>     
+                <?php */?>
+              </ul>
+              <?php else: 
+          
+                $event = 'hover';
+                $effect = 'fade';
+                $fullWidth = ',fullWidth: true';
+                $speed = 0;
+                $submenu_width = 200;
+                $menuwidth = 100;
+        
+              ?>
+            
+                <script type="text/javascript">
+                  var $ = jQuery;
+                  
+                  jQuery(document).ready(function($) {
+                    jQuery('#<?php echo 'item_main_menus'; ?> .menu').dcMegaMenu({
+                      rowItems: <?php echo $menuwidth; ?>,
+                      subMenuWidth: '<?php echo $submenu_width; ?>',
+                      speed: <?php echo $speed; ?>,
+                      effect: '<?php echo $effect; ?>',
+                      event: '<?php echo $event; ?>'
+                      <?php echo $fullWidth; ?>
                     });
-                  </script>
+                  });
+                </script>
 
-                  <div class="dcjq-mega-menu" id="<?php echo 'item_main_menus'; ?>">    
-                    <?php
-                      
-                      $menu_name = 'primary-auction-main-header';
+                <div class="dcjq-mega-menu" id="<?php echo 'item_main_menus'; ?>">    
+                  <?php
+                    
+                    $menu_name = 'primary-auction-main-header';
 
-                      if ( ( $locations = get_nav_menu_locations() ) && isset( $locations[ $menu_name ] ) ) 
-                        $nav_menu = wp_get_nav_menu_object( $locations[ $menu_name ] );         
-                        wp_nav_menu( array( 'fallback_cb' => '', 'menu' => $nav_menu, 'container' => false ) );
-                    ?>    
-                  </div>
-                <?php endif; ?>
-                <?php 
-                else:
-                //--------
-                endif;
-                ?>
-            </div>
+                    if ( ( $locations = get_nav_menu_locations() ) && isset( $locations[ $menu_name ] ) ) 
+                      $nav_menu = wp_get_nav_menu_object( $locations[ $menu_name ] );         
+                      wp_nav_menu( array( 'fallback_cb' => '', 'menu' => $nav_menu, 'container' => false ) );
+                  ?>    
+                </div>
+              <?php endif; ?>
+              <?php 
+              else:
+              //--------
+              endif;
+              ?>
+          </div>   
+        </div>
+      </div>
+    <?php if ( ! is_home() ) {?>
 
-          <div class="navbar-collapse collapse">        
-  					<div class="nav navbar-nav navbar-right">
+    <div class="container">
 
-                 
-                 <!-- main menu ending --> 
-  			       <div class="navbar-form navbar-left" id="site-search" role="search">
-  			        	<form method="get" action="<?php echo AuctionTheme_advanced_search_link(); ?>/" class="navbar-form" role="search">
-  			            <div class="input-group">
-  				            <?php
-  											if(AuctionTheme_using_permalinks() == false)
-  												echo '<input type="hidden" value="'.get_option('AuctionTheme_adv_search_id').'" name="page_id" />';
-  										?>
-  				            <input type="text" class="form-control" placeholder="Search Properties" name="srch-term" id="srch-term" onfocus="this.value=''" name="term" autocomplete="off" onkeyup="suggest(this.value);" 
-  				            onblur="fill();" />
-  				         		<div class="input-group-btn">
-  				         			<button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
-  				            	
-  				            </div>
-  				          </div>
-  			    			</form>
-  			            
-  			        	<div class="suggestionsBox" id="suggestions" style="z-index:999;display: none;"> 
-  			            <img src="<?php echo get_bloginfo('template_url');?>/images/arrow.png" style="position: relative; top: -12px; left: 30px;" alt="upArrow" />
-  			            <div class="suggestionList" id="suggestionsList"> &nbsp; </div>
-  			          </div>
-  			      </div>
-              <!-- end your search bar -->
-            </div>
-      </div>     
-    </div>
-</div>
-<?php if ( ! is_home() ) {?>
-
-<div class="container">
-
-<?php }?>
+    <?php }?>
 
       
